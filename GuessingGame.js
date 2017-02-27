4127bd47aea36407cadf07b6721ff3c057af27fb
chaimsash <me@chaimsash.com>
2017-02-26 22:09:35 -0500

    guessing game complete

--- GuessingGame.js
+++ GuessingGame.js
@@ -29,17 +29,29 @@ Game.prototype.isLower = function (){
 };
 
 Game.prototype.playersGuessSubmission = function(guess){
-    if (guess < 1 || guess > 100 || typeof(guess) !== "number"){
+    if (guess < 1 || guess > 100 || typeof(guess) !== "number" || isNaN(guess)){
+      $('h1').text("That is an invalid guess.");
       throw "That is an invalid guess.";
-    } else this.playersGuess = guess;
+    } else {
+      this.playersGuess = guess;
+      var index = game.pastGuesses.length + 1;
+      $('.guess li:nth-child(' + index + ')').text(guess);
+      if(game.isLower()){
+        $('h2').text('Guess a higher number.');
+      } else $('h2').text('Guess a lower number.');
+    }
     return this.checkGuess(guess);
 };
 
 Game.prototype.checkGuess = function(guess){
-  if (this.pastGuesses.length > 3){
-    return 'You Lose.';
-  } else if (this.winningNumber === guess){
+  if (this.winningNumber === guess){
+    $('#menu-btns button:last, #submit').prop("disabled",true);
+    $('h2').text("Press the Reset button to play again!");
     return 'You Win!';
+  } else if (this.pastGuesses.length > 3){
+    $('#menu-btns button:last, #submit').prop("disabled",true);
+    $('h2').text("Press the Reset button to play again!");
+    return 'You Lose.';
   } else if (this.pastGuesses.indexOf(guess) > -1){
     return 'You have already guessed that number.';
   } else this.pastGuesses.push(guess);
@@ -65,6 +77,32 @@ Game.prototype.provideHint = function(){
   return shuffle(resultArr);
 };
 
-$(document).on('ready', function() {
-   console.log('Everything is loaded!');
+$(document).ready(function() {
+  game = newGame();
+  console.log(game.winningNumber);
+  var orgTitle = $('h1').clone();
+  var orgSubTitle = $('h2').clone();
+  var orgList = $('.guess').clone();
+  var guessSubmit = function(){
+    var guess = +$('#player-input').val();
+    $('h1').text(game.playersGuessSubmission(guess));
+    $('#player-input').val('');
+  };
+  $('#submit').click(guessSubmit);
+  $('#player-input').keypress(function(e){
+    var key = e.which;
+    if (key == 13){
+      guessSubmit();
+    }
+  });
+  $('#menu-btns button:first').click(function(){
+    game = newGame();
+    $('h1').replaceWith($(orgTitle).clone());
+    $('h2').replaceWith($(orgSubTitle).clone());
+    $('.guess').replaceWith($(orgList).clone());
+    $('#menu-btns button:last, #submit').prop("disabled",false);
+  });
+  $('#menu-btns button:last').click(function(){
+    $('h1').text('One of these are the answer: ' + game.provideHint());
+  });
 });
